e82fcbbd8d7d4a0f70eaed88fb13b4fa8d5eb602
Pratik733 <pratik.tayade733@gmail.com>
2025-01-02 01:05:11 +0530

    vite-plugin-eslint issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+// @ts-expect-error eslint is not typed
 import eslint from 'vite-plugin-eslint';
 import { defineConfig } from "vite"
  
